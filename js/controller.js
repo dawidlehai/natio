545d43c19b1dbbc4691f5283ec94022c9085068b
dawidlehai <haidawidle@gmail.com>
2022-11-04 17:44:07 +0100

    Added saveProgress function and fixed logic in case of islands

--- js/controller.js
+++ js/controller.js
@@ -4,8 +4,6 @@ const controlCountries = async function () {
   try {
     const query = window.location.search.replace("?query=", "");
     await model.loadCountryAndNeighbours(query);
-    console.log(model.state.country);
-    console.log(model.state.neighbours);
   } catch (error) {
     console.log(error);
   }
--- js/model.js
+++ js/model.js
@@ -3,6 +3,24 @@ import { getData } from "./helpers.js";
 export const state = {
   country: {},
   neighbours: [],
+  progress: new Set(),
+  favourites: [],
+};
+
+const createSnippet = function (name, flag) {
+  return {
+    name,
+    flag,
+  };
+};
+
+const saveProgress = function () {
+  state.progress.add(
+    createSnippet(state.country.name.common, state.country.flags.png)
+  );
+  state.neighbours.forEach((country) =>
+    state.progress.add(createSnippet(country.name.common, country.flags.png))
+  );
 };
 
 const loadCountry = async function (country) {
@@ -24,9 +42,12 @@ const loadNeighbours = async function (...neighbours) {
 export const loadCountryAndNeighbours = async function (country) {
   try {
     await loadCountry(country);
+    if (state.country.borders) await loadNeighbours(...state.country.borders);
 
-    if (!state.country.borders.length) return;
-    await loadNeighbours(...state.country.borders);
+    saveProgress();
+    console.log(state.country);
+    console.log(state.neighbours);
+    console.log(state.progress);
   } catch (error) {
     throw error;
   }
